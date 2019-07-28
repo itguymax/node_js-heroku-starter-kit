a910946df4e7b1bb86ef704bc6461720f06a46c4
Apple <apple@Apples-MacBook-Air.local>
2019-07-28 09:38:43 +0100

    heroku setup in progress

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var cors = require('cors');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+app.use(cors());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (request, response) {
+  response.send('Hello World!')
+});
+
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
+  ]);
+});
+
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
